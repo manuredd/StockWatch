6e1c8f7ef52a7ade5d2d71f20f9c4b4a0d9f0a36
manuredd <74529341+manuredd@users.noreply.github.com>
2022-09-23 13:26:25 -0500

    Initial Commit

---
+++ StockWatch/StockWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockWatchApp.swift
+//  StockWatch
+//
+//  Created by Manu Redd on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct StockWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
